39531b650bf8239774277087867f0a9f7949db67
Luis felipe <84816784+Fel1pe95@users.noreply.github.com>
2021-05-29 23:01:49 -0300

    caminho padrão para criação de pasta de dados

--- src/model/Dao/impl/ClienteAcess.java
+++ src/model/Dao/impl/ClienteAcess.java
@@ -2,10 +2,12 @@ package model.Dao.impl;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
+
 import model.Dao.ClienteDao;
 import model.entidades.Cliente;
 import model.entidades.Conta;
@@ -14,7 +16,7 @@ public class ClienteAcess implements ClienteDao {
 
 	@Override
 	public void LeituraCliente(Map<Integer, Cliente> map) {
-		String caminho = "C:\\Users\\Andressa\\Desktop\\Programação\\Projeto movientacao bancaria\\Clientes\\Clientes.csv";
+		String caminho = "C:\\Clientes\\Clientes.csv";
 		try (BufferedReader brr = new BufferedReader(new FileReader(caminho))) {
 			String linha = brr.readLine();
 			while (linha != null) {
@@ -26,56 +28,45 @@ public class ClienteAcess implements ClienteDao {
 				Integer cpf = Integer.parseInt(campos[3]);
 				Integer senha = Integer.parseInt(campos[4]);
 				Double saldo = Double.parseDouble(campos[5]);
-
 				Cliente c = new Cliente(nome, cpf, new Conta(agencia, conta, senha, saldo));
-
 				map.put(senha, c);
-
 				linha = brr.readLine();
-
 			}
-
 			System.out.println("Programa carregado!");
-
 		} catch (IOException e) {
-			System.out.println("Erro: " + e.getMessage());
+			System.out.println("Instalando novo banco de dados...");
+			new File("C:\\Clientes").mkdir();
+			System.out.println("Novo banco criado! ");
 		}
-
 	}
 
 	@Override
-	public void gravacaoCLiente(String caminho, Map<Integer, Cliente>map) {
+	public void gravacaoCLiente(String caminho, Map<Integer, Cliente> map) {
 
 		try (BufferedWriter br = new BufferedWriter(new FileWriter(caminho))) {
-			for(Object obj : map.values()) {
+			for (Object obj : map.values()) {
 				br.write(obj.toString());
-				br.newLine();	
+				br.newLine();
 			}
-			
-			
-			System.out.println("Cliente cadastrado com sucesso!");
+			System.out.println("Programa finalizado!");
 		} catch (IOException e) {
 			System.out.println("Erro: " + e.getMessage());
 		}
-
 	}
 
 	@Override
 	public String caminho() {
-	return "C:\\Users\\Andressa\\Desktop\\Programação\\Projeto movientacao bancaria\\Clientes\\Clientes.csv";
+		return "C:\\Clientes\\Clientes.csv";  
 	}
 
 	@Override
-	public void inserirCliente(Map<Integer, Cliente>map) {
-		
-		try (BufferedWriter br = new BufferedWriter(new FileWriter(caminho(), true))) {	
-				br.write(map.toString());
-				br.newLine();	
+	public void inserirCliente(Map<Integer, Cliente> map) {
+		try (BufferedWriter br = new BufferedWriter(new FileWriter(caminho(), true))) {
+			br.write(map.toString());
+			br.newLine();
 			System.out.println("Cliente cadastrado com sucesso!");
 		} catch (IOException e) {
 			System.out.println("Erro: " + e.getMessage());
 		}
-		
 	}
-
 }
